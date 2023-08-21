16ed398987312f73cb308bb75b9e296e5840efcd
usman133 <usmankiani26@gmail.com>
2023-08-21 20:08:32 +1200

    pulling and displaying questions

--- game.js
+++ game.js
@@ -36,9 +36,30 @@ let questions = [
     },
 ];
 
-//CONSTRAINTS
+//CONSTANTS
 const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
 
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [ ... questions];
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach( choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+};
+
+startGame();
 
 
 
