c8b7bfbc63cf143e2465a3986be94cc80990dc20
usman133 <usmankiani26@gmail.com>
2023-08-28 19:55:55 +1200

    Save high scores in local storage

--- end.js
+++ end.js
@@ -2,6 +2,10 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+
 finalScore.innerText = mostRecentScore;
 
 // Check if the username is typed
@@ -11,4 +15,15 @@ username.addEventListener('keyup', () => {
 
 saveHighScore = e => {
     e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name:username.value
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5)
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/");
 }
